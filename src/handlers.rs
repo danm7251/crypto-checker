252048c396edfaf98377d0632fbc5cf6503fc897
Daniel Maynard <danielmaynard7@outlook.com>
2026-03-16 13:59:20 +0000

    Construct upstream request

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,18 +3,19 @@ use std::collections::HashMap;
 use worker::*;
 
 const SUPPORTED_FIAT: &[&str] = &["USD"];
+const UPSTREAM_PROVIDER: &str = "https://api.kraken.com/0/public/Ticker";
 
 pub async fn price(req: &Request) -> Result<Response> {
     // Extract query parameters from URL
     let params = query_params(req)?;
 
     // Validate query parameters
-    let _coin = match params.get("coin") {
+    let coin = match params.get("coin") {
         Some(coin) => coin,
         None => return Response::error("Missing required parameter: coin", 400),
     };
 
-    let _currency = match params.get("currency") {
+    let currency = match params.get("currency") {
         Some(currency) => {
             if !SUPPORTED_FIAT.contains(&currency.to_uppercase().as_str()) {
                 return Response::error("Unsupported currency", 400);
@@ -23,6 +24,17 @@ pub async fn price(req: &Request) -> Result<Response> {
         }
         None => return Response::error("Missing required parameter: currency", 400),
     };
+
+    // Construct upstream request
+    let uri = format!("{}?pair={}{}", UPSTREAM_PROVIDER, &coin, &currency);
+
+    let headers = Headers::new();
+    headers.set("Accept", "application/json")?;
+
+    let mut init = RequestInit::new();
+    init.with_headers(headers);
+
+    let _request = Request::new_with_init(&uri, &init)?;
 
     Response::error("Under development", 503)
 }
