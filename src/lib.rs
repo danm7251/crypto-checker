6276d6de090bb4f5de8fe1da1cddf4d8f387e8aa
Daniel Maynard <danielmaynard7@outlook.com>
2026-03-16 13:30:26 +0000

    The way rustfmt was formatting the routes was annoying

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,7 @@ use worker::*;
 #[event(fetch)]
 async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     Router::new()
-        .get_async(
-            "/price",
-            |req, _| async move { handlers::price(&req).await },
-        )
+        .get_async("/price", |req, _| async move { handlers::price(&req).await })
         .run(req, env)
         .await
 }
