9855c7d2abacc1c90d47057b5f68d276ae55e736
Daniel Maynard <danielmaynard7@outlook.com>
2026-04-05 10:55:18 +0100

    Removed unnecessary indentation

--- src/handlers/price.rs
+++ src/handlers/price.rs
@@ -1,41 +1,41 @@
-    use std::collections::HashMap;
-    use futures::future::join_all;
-    use worker::*;
+use std::collections::HashMap;
+use futures::future::join_all;
+use worker::*;
 
-    use crate::providers::{ALL_PROVIDERS, Provider};
+use crate::providers::{ALL_PROVIDERS, Provider};
 
-    const SUPPORTED_FIAT: &[&str] = &["USD"];
+const SUPPORTED_FIAT: &[&str] = &["USD"];
 
-    pub async fn price(req: &Request) -> Result<Response> {
-        // Extract query parameters from URL
-        let params = query_params(req)?;
+pub async fn price(req: &Request) -> Result<Response> {
+    // Extract query parameters from URL
+    let params = query_params(req)?;
 
-        // Validate query parameters
-        let coin = match params.get("coin") {
-            Some(coin) => coin,
-            None => return Response::error("Missing required parameter: coin", 400),
-        };
+    // Validate query parameters
+    let coin = match params.get("coin") {
+        Some(coin) => coin,
+        None => return Response::error("Missing required parameter: coin", 400),
+    };
 
-        // [!] Currently unnecessary
-        let _fiat = match params.get("currency") {
-            Some(fiat) => {
-                if !SUPPORTED_FIAT.contains(&fiat.to_uppercase().as_str()) {
-                    return Response::error("Unsupported currency", 400);
-                }
-                fiat
+    // [!] Currently unnecessary
+    let _fiat = match params.get("currency") {
+        Some(fiat) => {
+            if !SUPPORTED_FIAT.contains(&fiat.to_uppercase().as_str()) {
+                return Response::error("Unsupported currency", 400);
             }
-            None => return Response::error("Missing required parameter: currency", 400),
-        };
+            fiat
+        }
+        None => return Response::error("Missing required parameter: currency", 400),
+    };
 
-        let raw_results: Vec<Result<f64>> = serial_fetch(ALL_PROVIDERS, coin).await;
+    let raw_results: Vec<Result<f64>> = serial_fetch(ALL_PROVIDERS, coin).await;
 
-        let (avg_price, sources) = calculate_result(&raw_results);
+    let (avg_price, sources) = calculate_result(&raw_results);
 
-        Response::from_json(&serde_json::json!({
-            "average_price": avg_price,
-            "sources": sources,
-        }))
-    }
+    Response::from_json(&serde_json::json!({
+        "average_price": avg_price,
+        "sources": sources,
+    }))
+}
 
 fn calculate_result(results: &[Result<f64>]) -> (f64, u8) {
     let mut total_price = 0.0;
